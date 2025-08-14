0b33dacb94c52fba8ff92d37d53a2f4cf0214618
heetpateldev <heetpateldev@gmail.com>
2025-08-14 13:19:40 +0530

    Update 2

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -118,7 +118,6 @@ export const login = async (req , res)=>{
     }
 }
 
-
 export const logout = async (req , res)=>{
     try {
         res.clearCookie("jwt" , {
--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -10,41 +10,6 @@ export const getJudge0LanguageId = (language)=>{
     return languageMap[language.toUpperCase()]
 }
 
-const sleep  = (ms)=> new Promise((resolve)=> setTimeout(resolve , ms))
-
-export const pollBatchResults = async (tokens)=>{
-    while(true){
-        
-        const {data} = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`,{
-            params:{
-                tokens:tokens.join(","),
-                base64_encoded:false,
-            }
-        })
-
-        const results = data.submissions;
-
-        const isAllDone = results.every(
-            (r)=> r.status.id !== 1 && r.status.id !== 2
-        )
-
-        if(isAllDone) return results
-        await sleep(1000)
-    }
-}
-
-export const submitBatch = async (submissions)=>{
-    const {data} = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,{
-        submissions
-    })
-
-
-    console.log("Submission Results: ", data)
-
-    return data // [{token} , {token} , {token}]
-}
-
-
 export function getLanguageName(languageId){
     const LANGUAGE_NAMES = {
         74: "TypeScript",
@@ -54,4 +19,56 @@ export function getLanguageName(languageId){
     }
 
     return LANGUAGE_NAMES[languageId] || "Unknown"
+}
+
+export const submitBatch = async ( submission ) => {
+    
+    const options = {
+        method: 'POST',
+        url: `${process.env.SUBMISSION_POST_URL}`,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${process.env.SULU_AUTH_TOKEN}`
+        },
+        data: {
+            submissions: submission
+        }
+    }
+
+    const { data } = await axios( options );
+
+    return data;
+}
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export const pollBatchResult = async ( tokens ) => {
+    
+    const options = {
+        method: 'GET',
+        url: `${process.env.SUBMISSION_GET_URL}`,
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${process.env.SULU_AUTH_TOKEN}`
+        },
+        params: {
+            tokens: tokens.join(","),
+            base64_encoded: false,
+        }
+    };
+
+    while( true ){
+
+        const { data } = await axios.request( options );
+        const result = data.submissions;
+
+        const isAllDone = result.every(
+            (r) => r.status.id !== 1 && r.status.id !== 2
+        )
+
+        if (isAllDone) return result;
+
+        await sleep(1000);
+    }
 }
